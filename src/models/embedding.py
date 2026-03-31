94a717f2f53b034d5ffbf374330be7f3b7e4fd0d
Sai <pragnaan98@gmail.com>
2026-03-31 18:46:58 +0530

    embedding add

---
+++ src/models/embedding.py
@@ -0,0 +1,32 @@
+import torch
+import torch.nn as nn
+
+class GridEmbedding(nn.Module):
+    def __init__(self, d_model,max_size=30,max_pairs=12):
+        super().__init__()
+
+        self.color_embed=nn.Embedding(10,d_model)  ## TODO : VERIFY WITH LESS THAB d_model like 32 - 64 type shi
+        self.row_embed = nn.Embedding(max_size,d_model)
+        self.col_embed = nn.Embedding(max_size,d_model)
+        self.pair_embed =nn.Embedding(max_pairs,d_model)
+        self.io_embed=nn.Embedding(2, d_model)
+        
+    def forward(self, grid, pair_id=0, is_output=0):
+        H, W = grid.shape
+        device = grid.device
+
+        rows = torch.arange(H, device=device).unsqueeze(1).expand(H, W)
+        cols = torch.arange(W, device=device).unsqueeze(0).expand(H, W)
+
+        x = self.color_embed(grid)
+        x = x + self.row_embed(rows) + self.col_embed(cols)
+
+        # pair embedding
+        pair = torch.full((H, W), pair_id, device=device)
+        x = x + self.pair_embed(pair)
+
+        # IO embedding
+        io = torch.full((H, W), is_output, device=device)
+        x = x + self.io_embed(io)
+
+        return x
